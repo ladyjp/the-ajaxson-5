14edae732a7d0c00524d26cf1efc0cd25b0383d2
ladyjp <ladyjp_08@yahoo.com>
2019-01-30 18:01:41 -0600

    1st ajaxson commit

--- request-gif.js
+++ request-gif.js
@@ -13,29 +13,43 @@ $(document).ready(function() {
  * upon receiving a response from Giphy, updates the DOM to display the new GIF
  */
 function fetchAndDisplayGif(event) {
-    
+    var $body = $('body');
+
     // This prevents the form submission from doing what it normally does: send a request (which would cause our page to refresh).
     // Because we will be making our own AJAX request, we dont need to send a normal request and we definitely don't want the page to refresh.
     event.preventDefault();
     
     // get the user's input text from the DOM
-    var searchQuery = ""; // TODO should be e.g. "dance"
-
+    var searchQuery = $('#tag').val(); // TODO should be e.g. "dance"
+    
     // configure a few parameters to attach to our request
     var params = { 
         api_key: "dc6zaTOxFJmzC", 
-        tag : "" // TODO should be e.g. "jackson 5 dance"
+        tag : "Jackson 5", searchQuery, // TODO should be e.g. "jackson 5 dance"
     };
     
-    // make an ajax request for a random GIF
+    var giphyUrl = 'https://api.giphy.com/v1/gifs/random';
     $.ajax({
-        url: "", // TODO where should this request be sent?
+        url: giphyUrl, // TODO where should this request be sent?
         data: params, // attach those extra parameters onto the request
+        dataType: "jsonp",
         success: function(response) {
+            var riddle = $('#tag2').val()
+            if (riddle == '5'){
+                $body.text('Just one second while we load your gif!');
+                
+                var image = response = image_url
+                $("#gif").attr("src", response);
+            } 
+            else {$body.text('No Gif for you!');
+            }
+
+           
+            
             // if the response comes back successfully, the code in here will execute.
             
             // jQuery passes us the `response` variable, a regular javascript object created from the JSON the server gave us
-            console.log("we received a response!");
+            //console.log("we received a response!");
             console.log(response);
             
             // TODO
